08b9a8987e11ca18947f5f36c27a9c461ec7645e
Taras Barninets <barninets@gmail.com>
2020-01-26 00:52:09 +0200

    Add speed test result parser and visualizer
    Change-Id: I03da6c93cab13bbde1c9ea2709ac48269d40adb5

---
+++ src/SpeedTestParser.h
@@ -0,0 +1,29 @@
+#ifndef SPEED_TEST_PARSER_H
+#define SPEED_TEST_PARSER_H
+
+#include <QObject>
+#include <QVector>
+#include <QDateTime>
+
+class SpeedTestParser : public QObject
+{
+     Q_OBJECT
+
+public:
+    virtual bool createDocument(QString path) = 0;
+    virtual void parseDocument() = 0;
+    virtual int getCountObject() = 0;
+
+    const QVector<double>& getDownloadData() const noexcept { return mDownloadData; }
+    const QVector<double>& getUploadData()  const noexcept { return mUploadData; }
+    const QVector<double>& getPingData()  const noexcept { return mPingData; }
+    const QVector<QDateTime>& getTimestampData()  const noexcept { return mTimestampData; }
+
+protected:
+    QVector<double> mDownloadData;
+    QVector<double> mUploadData;
+    QVector<double> mPingData;
+    QVector<QDateTime> mTimestampData;
+};
+
+#endif // SPEEDTESTPARSER_H
---
+++ src/UserWindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "SpeedTestJsonParser.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class UserWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    UserWindow(QWidget *parent = nullptr);
+    virtual ~UserWindow();
+
+private:
+    void fillTableView(QStandardItemModel* model);
+    void drawChart();
+
+    Ui::MainWindow *mUi;
+    QStandardItemModel* mModel;
+    SpeedTestParser* mParser;
+
+private slots:
+    void chooseFile();
+
+};
+
+#endif // MAINWINDOW_H
